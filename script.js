1a008fd286f9983eed876a9ab0b14fdae615a913
kkseenniiaa <98638900+kkseenniiaa@users.noreply.github.com>
2023-04-10 17:41:11 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,64 +1,61 @@
-let taskInput = document.querySelector('#taskInput'); //input
-let addBtn = document.querySelector('#addBtn');  //btn
+let taskInput = document.querySelector('#taskInput');
+let addBtn = document.querySelector('#addBtn');
 let list = document.querySelector('#list');
-
+let tasks = [];
 if (localStorage.getItem('htmlsLs')) {
     list.innerHTML = localStorage.getItem('htmlLs');
 }
 
+addBtn.addEventListener('click', function () {
+    let elem = document.createElement('li');
+    addTask(elem);
+    list.append(elem);
+    taskInput.value = '';
+})
+
 function addTask(elem) {
     elem.classList.add('item');
-    elem.textContent = taskinput.value;
+    elem.textContent = taskInput.value;
     let itemBtns = document.createElement('div');
     elem.append(itemBtns);
-    itemBtns.className = 'item_btns';
+    itemBtns.className = 'item__btns';
 
     let donebtn = document.createElement('i');
     donebtn.className = "fa-regular fa-square-check";
     itemBtns.append(donebtn);
     donebtn.setAttribute('data-action', 'complete');
 
-    let deletebtn = document.createElement('i');
-    deletebtn.className = 'fa-solid fa-trash-can';
-    itemBtns.append(deleteButton);
-    deleteButton.setAttribute('data-action', 'delete');
+    let deleteBtn = document.createElement('i');
+    deleteBtn.className = 'fa-solid fa-trash-can';
+    itemBtns.append(deleteBtn);
+    deleteBtn.setAttribute('data-action', 'delete');
 }
 
-function comleteBtn() {
+let newTask = {
+    id: Date.now(),
+    text: taskInput.value,
+    complete: false
+}
+
+function completeBtn(target) {
     target.closest('li').classList.toggle('done');
     localStorage.setItem('htmlsLs', list.innerHTML);
 }
 
-function removeTask() {
+function removeTask(target) {
     target.closest('li').remove();
     taskInput.value = '';
-    localStorage.setItem('htmlsLs', list.innerHTML);
+    let index = tasks.findIndex((task) => {
+        return task.id == target.closest('li').id
+    });
 }
 
-
-/*list.addEventListener('click', (event) => {
+list.addEventListener('click', (event) => {
     target = event.target;
-    if (target.dataset.ac);
-
-
-
-
-
-        
-
-    let item = document.createElement('div');
-    item.className = 'item_btns';
-
-    chbox.addEventListener('click', function () {
-        elem.classList.toggle('done');
-    })
-
-    let btn1 = document.createElement('i')
-    btn1.className = 'fa-solid fa-trash-can';
-    item.append(btn1);
-
-    btn1.addEventListener('click', function () {
-        list.removeChild(elem);
-    })
-
-})*/
+    if (target.dataset.action == 'complete') {
+        completeBtn(target);
+    };
+    if (target.dataset.action == 'delete') {
+        removeTask(target);
+    };
+})
